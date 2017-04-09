2551ad997a79474131036b1690bb7305f4916ce7
lehuuhienn <!@#Hien456>
2017-04-09 15:04:26 +0700

    update HTML HIEN/css/bootstrap/bootstrap-theme.css

---
+++ HTML HIEN/js/main.js
@@ -0,0 +1,12 @@
+// menu 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// slider
+
