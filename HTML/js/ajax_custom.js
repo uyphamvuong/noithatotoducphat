a5ae06275fa9fec56d9ebf93e2a7549564fd3f34
Phạm Vương Uy <uyphamvuong@gmail.com>
2017-04-09 16:08:21 +0700

    HTML trở lại

---
+++ HTML/js/ajax_custom.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+	
+	$('.click_showsp').click(function(event) {
+		$('.click_showsp').removeClass('active');
+		var id = $(this).data('id');
+		$("#loader").fadeIn(3000);
+		$("#hien_sp").css('display', 'none');
+		$(this).addClass('active');
+		$.ajax({
+			url: './ajax/load_sp.php',
+			type: 'POST',
+			data: {id: id},
+			success: function(msg){
+				$("#loader").fadeOut(1000, function() {
+					$("#hien_sp").fadeIn(1000).html(msg);
+				}); 
+			}
+		})
+	});
+});
