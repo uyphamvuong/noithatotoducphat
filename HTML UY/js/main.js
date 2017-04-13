460183e9f6502ef89fc0fdc2e041b4e0db51615b
fsg <fsg>
2017-04-14 00:23:23 +0700

    Update tới video

--- HTML UY/js/main.js
+++ HTML UY/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     ScrollToTop();
     StickyMenuOnTop();
+    Core_revsliderHome();
 
 });
 
@@ -42,4 +43,80 @@ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 // StickyMenu On Top
 function StickyMenuOnTop() {
     $('.navbar-home').scrollToFixed();
+}
+
+var revapi2;
+
+function Core_revsliderHome() {
+    if ($('#rev_sliderHome').length <= 0) { return; }
+    if ($('#rev_sliderHome').revolution == undefined)
+        revslider_showDoubleJqueryError('#rev_sliderHome');
+    else
+        revapi2 = $('#rev_sliderHome').show().revolution(
+         {
+             dottedOverlay: "none",
+             delay: 9000,
+             startwidth: 1900,
+             startheight: 600,
+             hideThumbs: 200,
+
+             thumbWidth: 100,
+             thumbHeight: 50,
+             thumbAmount: 2,
+
+             navigationType: "bullet",
+             navigationArrows: "solo",
+             navigationStyle: "round",
+
+             touchenabled: "on",
+             onHoverStop: "on",
+
+             swipe_velocity: 0.7,
+             swipe_min_touches: 1,
+             swipe_max_touches: 1,
+             drag_block_vertical: false,
+
+             keyboardNavigation: "off",
+
+             navigationHAlign: "center",
+             navigationVAlign: "bottom",
+             navigationHOffset: 0,
+             navigationVOffset: 20,
+
+             soloArrowLeftHalign: "left",
+             soloArrowLeftValign: "center",
+             soloArrowLeftHOffset: 20,
+             soloArrowLeftVOffset: 0,
+
+             soloArrowRightHalign: "right",
+             soloArrowRightValign: "center",
+             soloArrowRightHOffset: 20,
+             soloArrowRightVOffset: 0,
+
+             shadow: 0,
+             fullWidth: "on",
+             fullScreen: "off",
+
+             spinner: "spinner0",
+
+             stopLoop: "off",
+             stopAfterLoops: -1,
+             stopAtSlide: -1,
+
+             shuffle: "off",
+
+             autoHeight: "off",
+             forceFullWidth: "off",
+
+             hideThumbsOnMobile: "off",
+             hideNavDelayOnMobile: 1500, hideBulletsOnMobile: "off",
+             hideArrowsOnMobile: "off",
+             hideThumbsUnderResolution: 0,
+
+             hideSliderAtLimit: 0,
+             hideCaptionAtLimit: 0,
+             hideAllCaptionAtLilmit: 0,
+             startWithSlide: 0,
+             fullScreenOffsetContainer: ""
+         });
 }
